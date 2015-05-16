bd9c6d810cbc283b846680c810cf48a853cbe087
Tim Brandin <tim.brandin@gmail.com>
2015-05-16 21:59:15 +0200

    Added deploy script.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
   Template.content.onRendered(function() {
-    this.$('li').click(function() {
+    this.$('li').click(function(e) {
       e.preventDefault();
       var href = $('a', this).attr('href');
       window.open(href, '_blank');

f3d4721b33d39adf2fa6904d22549214bb101990
Tim Brandin <tim.brandin@gmail.com>
2015-05-19 09:15:38 +0200

    Moving slack-invite into a package.

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
 if (Meteor.isClient) {
-  Template.content.onCreated(function() {
-    var instance = this;
-    instance.invite = new ReactiveVar(false);
-    instance.error = new ReactiveVar("");
-    instance.subscribe('slack');
-  });
-
-  Template.content.helpers({
-    invite: function() {
-      var instance = Template.instance();
-      return instance.invite.get();
-    },
-
-    slack: function() {
-      return Slack.findOne();
-    },
-
-    error: function() {
-      var instance = Template.instance();
-      return instance.error.get();
-    }
-  });
-
   Template.content.onRendered(function() {
     this.$('li').click(function(e) {
       e.preventDefault();
@@ -29,23 +6,4 @@ if (Meteor.isClient) {
       window.open(href, '_blank');
     });
   });
-
-  Template.content.events({
-    'submit form': function(e, instance) {
-      e.preventDefault();
-      var email = instance.find('input').value;
-      Meteor.call('invite', email, function(err, res) {
-        if (!err && res.ok) {
-          instance.error.set("");
-          instance.invite.set(true);
-        }
-        else if (!err){
-          instance.error.set(res.error);
-        }
-        else {
-          instance.error.set('Someting is broken here.');
-        }
-      });
-    }
-  })
 }

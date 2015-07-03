f5bf46d553b84b40374cd3826097f57007458433
Tim Brandin <tim.brandin@gmail.com>
2015-07-03 08:45:26 +0200

    Update router.jsx

--- router.jsx
+++ router.jsx
@@ -1,10 +1,3 @@
-FlowRouter.triggers.enter([function(context) {
-  window.onload = function() {
-    ga && ga('send', 'pageview');
-    ga && ga('set', 'page', context.path);
-  };
-}]);
-
 FlowRouter.route("/", {
   subscriptions: function() {
     this.register('github', Meteor.subscribe('github'));
